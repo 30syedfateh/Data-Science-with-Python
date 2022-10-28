3a4f011e7274a33643282ef02e0d7b69d53a850f
30syedfateh <115559717+30syedfateh@users.noreply.github.com>
2022-10-28 18:45:05 +0530

    loops
    loops

---
+++ divisible.py
@@ -0,0 +1,5 @@
+for in i range(2000,4200):
+    if (i%7==0) and (i%5!=0):
+        print("This number is the divider of seven and can't be divided by five:",i)
+    else:
+        print("Fuck off:")
